e6712ee74dab3206d0200873a9711e9714548eed
Uzo Olisemeka <uzo@designbymobius.ca>
2019-04-17 13:38:30 -0400

    fix: properly ensure mail message's type prop is request

--- modules/detect-request.js
+++ modules/detect-request.js
@@ -2,7 +2,7 @@ module.exports = function( citizen ){
 
   citizen.hook.add( 'citizen-mail', 'detect-citizen-request', function( envelope, handle_mail ){
     if( ! envelope.msg.hasOwnProperty( 'type' ) ) return;
-    if( ! envelope.msg.type !== 'request' ) return;
+    if( envelope.msg.type !== 'request' ) return;
 
     if( ! envelope.msg.hasOwnProperty( 'request' ) ) return;
     if( ! envelope.msg.hasOwnProperty( 'id' ) ) return;
